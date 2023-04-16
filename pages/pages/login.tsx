164f4560c4d0a43e314b664c0e7ebf4d472dc53d
Dream4ever <cnhwvista@gmail.com>
2023-04-16 11:48:14 +0800

    feat: 登录页增加网站标题，并调整样式

--- pages/pages/login.tsx
+++ pages/pages/login.tsx
@@ -10,8 +10,9 @@ function LoginPage() {
   const imgSource = mode === 'dark' ? '/assets/img/login-office-dark.jpeg' : '/assets/img/login-office.jpg'
 
   return (
-    <div className='flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900'>
-      <div className='flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800'>
+    <div className='flex flex-col justify-center items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900'>
+      <div className='mb-16 text-center text-4xl font-bold'>供应链溯源管理系统</div>
+      <div className='h-full w-1/2 mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800'>
         <div className='flex flex-col overflow-y-auto md:flex-row'>
           <div className='relative h-32 md:h-auto md:w-1/2'>
             <Image
