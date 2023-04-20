02a0f02b191f199bbed09060db01410e1d7d1e5a
Dream4ever <cnhwvista@gmail.com>
2023-04-20 23:57:25 +0800

    feat: 完成用户身份的申请

--- pages/pages/index.tsx
+++ pages/pages/index.tsx
@@ -1,191 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import { Doughnut, Line } from 'react-chartjs-2'
+import React, { useState } from 'react'
 
-import CTA from 'components/CTA'
-import InfoCard from 'components/Cards/InfoCard'
-import ChartCard from 'components/Chart/ChartCard'
-import ChartLegend from 'components/Chart/ChartLegend'
 import PageTitle from 'components/Typography/PageTitle'
-import RoundIcon from 'components/RoundIcon'
 import Layout from 'containers/Layout'
-import response, { ITableData } from 'utils/demo/tableData'
-import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from 'icons'
 
 import {
-  TableBody,
-  TableContainer,
-  Table,
-  TableHeader,
-  TableCell,
-  TableRow,
-  TableFooter,
-  Avatar,
-  Badge,
-  Pagination,
+  Button,
+  Dropdown,
+  DropdownItem,
 } from '@roketid/windmill-react-ui'
 
-import {
-  doughnutOptions,
-  lineOptions,
-  doughnutLegends,
-  lineLegends,
-} from 'utils/demo/chartsData'
-
-import {
-  Chart,
-  ArcElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js'
+import { setRole } from 'api/user'
 
 function Dashboard() {
-  Chart.register(
-    ArcElement,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  )
+  const [roleId, setRoleId] = useState<number>(2)
+  const [isOpen, setIsOpen] = useState(false)
 
-  const [page, setPage] = useState(1)
-  const [data, setData] = useState<ITableData[]>([])
+  const roleList = ['生产商', '流通商', '待选择']
 
-  // pagination setup
-  const resultsPerPage = 10
-  const totalResults = response.length
-
-  // pagination change control
-  function onPageChange(p: number) {
-    setPage(p)
+  function toggleDropdown() {
+    setIsOpen(!isOpen)
   }
 
-  // on page change, load new sliced data
-  // here you would make another server request for new data
-  useEffect(() => {
-    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
-  }, [page])
+  function onRoleIdChange(id: number) {
+    setRoleId(id)
+    setIsOpen(false)
+  }
+
+  async function reqRole() {
+    const result = await setRole(localStorage.getItem('signUserId')!, roleId)
+    console.log(result.data)
+  }
 
   return (
     <Layout>
-      <PageTitle>Dashboard</PageTitle>
+      <PageTitle>设置用户角色</PageTitle>
+      <div className="relative flex flex-col flex-wrap">
+        <div className='flex items-center gap-x-4'>
+          <Button onClick={toggleDropdown} aria-label="Notifications" aria-haspopup="true">
+            选择用户角色
+          </Button>
+          <span>当前用户角色：{roleList[roleId]}</span>
+        </div>
 
-      <CTA />
-
-      {/* <!-- Cards --> */}
-      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-        <InfoCard title="Total clients" value="6389">
-          {/* @ts-ignore */}
-          <RoundIcon
-            icon={PeopleIcon}
-            iconColorClass="text-orange-500 dark:text-orange-100"
-            bgColorClass="bg-orange-100 dark:bg-orange-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="Account balance" value="$ 46,760.89">
-          {/* @ts-ignore */}
-          <RoundIcon
-            icon={MoneyIcon}
-            iconColorClass="text-green-500 dark:text-green-100"
-            bgColorClass="bg-green-100 dark:bg-green-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="New sales" value="376">
-          {/* @ts-ignore */}
-          <RoundIcon
-            icon={CartIcon}
-            iconColorClass="text-blue-500 dark:text-blue-100"
-            bgColorClass="bg-blue-100 dark:bg-blue-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="Pending contacts" value="35">
-          {/* @ts-ignore */}
-          <RoundIcon
-            icon={ChatIcon}
-            iconColorClass="text-teal-500 dark:text-teal-100"
-            bgColorClass="bg-teal-100 dark:bg-teal-500"
-            className="mr-4"
-          />
-        </InfoCard>
+        <Dropdown className='bg-white' isOpen={isOpen} onClose={() => { }}>
+          <DropdownItem onClick={() => onRoleIdChange(0)}>
+            <span>{roleList[0]}</span>
+          </DropdownItem>
+          <DropdownItem onClick={() => onRoleIdChange(1)}>
+            <span>{roleList[1]}</span>
+          </DropdownItem>
+        </Dropdown>
       </div>
 
-      <TableContainer>
-        <Table>
-          <TableHeader>
-            <tr>
-              <TableCell>Client</TableCell>
-              <TableCell>Amount</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Date</TableCell>
-            </tr>
-          </TableHeader>
-          <TableBody>
-            {data.map((user, i) => (
-              <TableRow key={i}>
-                <TableCell>
-                  <div className="flex items-center text-sm">
-                    <Avatar
-                      className="hidden mr-3 md:block"
-                      src={user.avatar}
-                      alt="User image"
-                    />
-                    <div>
-                      <p className="font-semibold">{user.name}</p>
-                      <p className="text-xs text-gray-600 dark:text-gray-400">
-                        {user.job}
-                      </p>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <span className="text-sm">$ {user.amount}</span>
-                </TableCell>
-                <TableCell>
-                  <Badge type={user.status}>{user.status}</Badge>
-                </TableCell>
-                <TableCell>
-                  <span className="text-sm">
-                    {new Date(user.date).toLocaleDateString()}
-                  </span>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <TableFooter>
-          <Pagination
-            totalResults={totalResults}
-            resultsPerPage={resultsPerPage}
-            label="Table navigation"
-            onChange={onPageChange}
-          />
-        </TableFooter>
-      </TableContainer>
-
-      <PageTitle>Charts</PageTitle>
-      <div className="grid gap-6 mb-8 md:grid-cols-2">
-        <ChartCard title="Revenue">
-          <Doughnut {...doughnutOptions} />
-          <ChartLegend legends={doughnutLegends} />
-        </ChartCard>
-
-        <ChartCard title="Traffic">
-          <Line {...lineOptions} />
-          <ChartLegend legends={lineLegends} />
-        </ChartCard>
+      <div className='mt-12'>
+        <Button disabled={roleId === roleList.length - 1} onClick={reqRole}>
+          申请
+        </Button>
       </div>
     </Layout>
   )
